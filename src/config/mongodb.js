696331202de27c26258284d685a0fda5760796ce
Nguyen Thai Duong <nguyenthaiduong17082003@gmail.com>
2026-04-14 10:42:00 +0700

    connect mongodb env variables

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,1 +1,24 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+import { env } from "~/config/environment";
 
+let kanbanDatabaseInstance = null;
+
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+export const CONNECT_DB = async () => {
+  await mongoClientInstance.connect();
+
+  kanbanDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME);
+};
+
+export const GET_DB = () => {
+  if (!kanbanDatabaseInstance) throw new Error("Must connect to Database first!");
+
+  return kanbanDatabaseInstance;
+};
