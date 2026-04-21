982e7bdf7fcb16b63acbc4c51333d4abb5783ecb
Nguyen Thai Duong <nguyenthaiduong17082003@gmail.com>
2026-04-21 17:08:12 +0700

    board controller

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,7 +1,7 @@
 import Joi from "joi";
 import { StatusCodes } from "http-status-codes";
 
-const createNew = async (req, res) => {
+const createNew = async (req, res, next) => {
   const correctCondition = Joi.object({
     title: Joi.string().required().min(3).max(50).trim().strict(),
     description: Joi.string().required().min(3).max(256).trim().strict(),
@@ -9,7 +9,7 @@ const createNew = async (req, res) => {
 
   try {
     await correctCondition.validateAsync(req.body, { abortEarly: false });
-    res.status(StatusCodes.CREATED).json({ message: "APIs create list boards" });
+    next();
   } catch (error) {
     res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
       errors: new Error(error).message,
