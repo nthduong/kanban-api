1b6928dcb6ab2347feae837dffd89bc16d0782f5
Nguyen Thai Duong <nguyenthaiduong17082003@gmail.com>
2026-04-21 20:23:24 +0700

    mode dev production

--- src/middlewares/errorHandlingMiddleware.js
+++ src/middlewares/errorHandlingMiddleware.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import { StatusCodes } from "http-status-codes";
-// import { env } from '~/config/environment'
+import { env } from '~/config/environment'
 
 export const errorHandlingMiddleware = (err, req, res, next) => {
   if (!err.statusCode) err.statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
@@ -11,7 +11,7 @@ export const errorHandlingMiddleware = (err, req, res, next) => {
     stack: err.stack,
   };
 
-  // if (env.BUILD_MODE !== 'dev') delete responseError.stack
+  if (env.BUILD_MODE !== 'dev') delete responseError.stack
 
   res.status(responseError.statusCode).json(responseError);
 };
