f0618320923f95183a5ebdb230bdc17f7cffb4f2
Nguyen Thai Duong <nguyenthaiduong17082003@gmail.com>
2026-06-10 19:18:33 +0700

    fix(cors): allow requests without Origin header

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,25 +1,26 @@
-import { WHITELIST_DOMAINS } from '~/utils/constants'
-import { env } from '~/config/environment'
-import { StatusCodes } from 'http-status-codes'
-import ApiError from '~/utils/ApiError'
-
+import { WHITELIST_DOMAINS } from "~/utils/constants";
+import { env } from "~/config/environment";
+import { StatusCodes } from "http-status-codes";
+import ApiError from "~/utils/ApiError";
 
 export const corsOptions = {
   origin: function (origin, callback) {
+    if (env.BUILD_MODE === "dev") {
+      return callback(null, true);
+    }
 
-    if (env.BUILD_MODE === 'dev') {
-      return callback(null, true)
+    if (!origin) {
+      return callback(null, true);
     }
 
     if (WHITELIST_DOMAINS.includes(origin)) {
-      return callback(null, true)
+      return callback(null, true);
     }
 
-
-    return callback(new ApiError(StatusCodes.FORBIDDEN, `${origin} not allowed by our CORS Policy.`))
+    return callback(new ApiError(StatusCodes.FORBIDDEN, `${origin} not allowed by our CORS Policy.`));
   },
 
   optionsSuccessStatus: 200,
 
-  credentials: true
-}
+  credentials: true,
+};
